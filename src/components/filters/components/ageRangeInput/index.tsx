48bb185f760e66f42af21dd064740a1d3f979325
ArturoDucasse <ArturoM.Ducasse@gmail.com>
2025-03-09 18:48:19 -0400

    feat, add debounce to age range input component

--- src/components/filters/components/ageRangeInput/index.tsx
+++ src/components/filters/components/ageRangeInput/index.tsx
@@ -1,18 +1,24 @@
 import { Input } from "pixel-retroui";
 import { AgeRangeInputProps } from "./types";
 import { MIN_AGE, MAX_AGE, INPUT_STEP } from "./constants";
+import { useDebounce } from "../../../../utils/debounce";
 
-export const AgeRangeInput = ({ label, onChange }: AgeRangeInputProps) => (
-  <label className="text-left flex flex-col" htmlFor={label}>
-    {label}:
-    <Input
-      className="w-28"
-      onChange={onChange}
-      name={label}
-      type="number"
-      min={MIN_AGE}
-      max={MAX_AGE}
-      step={INPUT_STEP}
-    />
-  </label>
-);
+export const AgeRangeInput = ({ label, onChange }: AgeRangeInputProps) => {
+  const safeOnChange = onChange ?? (() => {}); // Handle possible undefined
+  const debounceOnChange = useDebounce(safeOnChange, 500);
+
+  return (
+    <label className="text-left flex flex-col" htmlFor={label}>
+      {label}:
+      <Input
+        className="w-28 "
+        onChange={({ target: { value } }) => debounceOnChange?.(value)}
+        name={label}
+        type="number"
+        min={MIN_AGE}
+        max={MAX_AGE}
+        step={INPUT_STEP}
+      />
+    </label>
+  );
+};
