7e5e8759adee5af4786e3f1b79c2ee47823426ae
ArturoDucasse <ArturoM.Ducasse@gmail.com>
2025-03-11 02:04:08 -0400

    feat, add dog/location types

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export type Dog = {
+  img: string;
+  name: string;
+  age: number;
+  breed: string;
+  zip_code: string;
+  id: string;
+};
+
+export type Location = {
+  city: string;
+  latitude: number;
+  county: string;
+  state: string;
+  zip_code: string;
+  longitude: number;
+};
