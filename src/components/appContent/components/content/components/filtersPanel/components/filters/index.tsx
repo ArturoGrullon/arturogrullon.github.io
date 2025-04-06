23b09738a4c1c365030aa9e4a8fef3591a6886c8
ArturoDucasse <ArturoM.Ducasse@gmail.com>
2025-04-06 03:29:41 -0400

    feat, add filters component

---
+++ src/components/appContent/components/content/components/filtersPanel/components/filters/index.tsx
@@ -0,0 +1,49 @@
+import { Card } from "pixel-retroui";
+import { AutoComplete } from "./components/autoComplete";
+import { Input } from "./components/input";
+import { FiltersProps } from "./types";
+import { FilterBy } from "./components/filterBy";
+import { useFetch } from "./hooks/useFetch";
+
+export const Filters = ({ handleAddFilter, activeFilters }: FiltersProps) => {
+  const { data: breeds = [] } = useFetch("/dogs/breeds");
+
+  return (
+    <Card className="flex flex-col gap-6 p-4 sticky top-[7rem] m-[5rem]">
+      <h1>Filters</h1>
+      <FilterBy {...{ handleAddFilter, activeFilters }} />
+      <AutoComplete
+        {...{
+          label: "Breed",
+          options: breeds,
+          activeOptions: activeFilters.breeds,
+          onBlur: (breeds: string[]) => handleAddFilter("breeds", breeds),
+        }}
+      />
+      <Input
+        placeholder="Press enter to add"
+        label={"Zip Code"}
+        type="number"
+        onPressEnter={(zipCode) => handleAddFilter("zipCodes", zipCode)}
+      />
+      <section className="flex gap-8">
+        <Input
+          className="w-28"
+          label="Min Age"
+          value={activeFilters.ageMin}
+          type="number"
+          onChange={(ageMin) => handleAddFilter("ageMin", ageMin)}
+          placeholder="Min"
+        />
+        <Input
+          className="w-28"
+          label="Max Age"
+          value={activeFilters.ageMax}
+          placeholder="Max"
+          type="number"
+          onChange={(ageMax) => handleAddFilter("ageMax", ageMax)}
+        />
+      </section>
+    </Card>
+  );
+};
