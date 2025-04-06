6563dccb429e8d5bc9fa6649b61467497e4e14f9
ArturoDucasse <ArturoM.Ducasse@gmail.com>
2025-04-06 03:26:37 -0400

    refactor, modify login input format

--- src/components/loginWrapper/components/Login/index.tsx
+++ src/components/loginWrapper/components/Login/index.tsx
@@ -1,42 +1,39 @@
 import { Button, Card, Input } from "pixel-retroui";
 import { motion } from "motion/react";
-import { usePassword } from "../../hooks/usePassword";
 import { LoginProps } from "./types";
+import { MIN_INPUT_CHARACTERS } from "./constants";
 
-export const Login = ({ handleLogin }: LoginProps) => {
-  const { passwordInputType, passwordInputIcon, togglePasswordVisibility } =
-    usePassword();
-
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-      <Card className="justify-center items-center flex" bg="gray">
-        <form
-          onSubmit={handleLogin}
-          className="flex p-20 flex-col gap-8 justify-center items-center"
-        >
-          <Input
-            className="w-[20rem]"
-            type="text"
-            name="username"
-            placeholder="Username"
-          />
-          <Input
-            className="w-[20rem]"
-            name="password"
-            placeholder="Password"
-            type={passwordInputType}
-            icon={passwordInputIcon}
-            onIconClick={togglePasswordVisibility}
-          />
-          <Button type="submit" className="w-28">
-            Login
-          </Button>
-        </form>
-      </Card>
-    </motion.div>
-  );
-};
+export const Login = ({ handleLogin }: LoginProps) => (
+  <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+  >
+    <Card className="justify-center items-center flex" bg="gray">
+      <form
+        onSubmit={handleLogin}
+        className="flex p-20 flex-col gap-8 justify-center items-center"
+      >
+        <Input
+          className="w-[20rem]"
+          type="text"
+          name="name"
+          required
+          placeholder="Username"
+          minLength={MIN_INPUT_CHARACTERS}
+        />
+        <Input
+          className="w-[20rem]"
+          name="email"
+          required
+          placeholder="Email"
+          type={"email"}
+          minLength={MIN_INPUT_CHARACTERS}
+        />
+        <Button type="submit" className="w-28">
+          Login
+        </Button>
+      </form>
+    </Card>
+  </motion.div>
+);
