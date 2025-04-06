37e84fa3d3d0493ac5b13b8741d4a6418da7c82f
ArturoDucasse <ArturoM.Ducasse@gmail.com>
2025-04-06 03:39:03 -0400

    refactor, properly fetch user information

--- src/components/loginWrapper/utils/handleLoginUser/index.ts
+++ src/components/loginWrapper/utils/handleLoginUser/index.ts
@@ -1,11 +1,23 @@
 import { FormEvent } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 import { User } from "../../types";
 
 export const handleLoginUser =
   (setUser: (user: User) => void) =>
-  (formEvent: FormEvent<HTMLFormElement>) => {
-    formEvent.preventDefault();
-    const formData = new FormData(formEvent.currentTarget);
-    const userCredentials = Object.fromEntries(formData.entries()) as User;
-    setUser(userCredentials);
+  async (formEvent: FormEvent<HTMLFormElement>) => {
+    try {
+      formEvent.preventDefault();
+      const formData = new FormData(formEvent.currentTarget);
+      const user = Object.fromEntries(formData.entries()) as User;
+      const url = `${import.meta.env.VITE_BASE_URL}/auth/login`;
+      await axios.post(url, user, { withCredentials: true });
+      toast.success("Login successful");
+      setUser(user);
+    } catch (error) {
+      if (!navigator.onLine) return toast.error("No internet connection");
+
+      if (axios.isAxiosError(error))
+        toast.error("Invalid format, please update your credentials");
+    }
   };
