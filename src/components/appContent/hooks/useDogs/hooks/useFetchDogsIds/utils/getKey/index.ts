a4e19fbd523b34e43e82d27fcc64e95b2c84346d
ArturoDucasse <ArturoM.Ducasse@gmail.com>
2025-04-06 23:14:20 -0400

    fix, pagination url

--- src/components/appContent/hooks/useDogs/hooks/useFetchDogsIds/utils/getKey/index.ts
+++ src/components/appContent/hooks/useDogs/hooks/useFetchDogsIds/utils/getKey/index.ts
@@ -5,8 +5,9 @@ export const getKey =
   (pageIndex: number, previousPageData: DogsSearch) => {
     const dogsSearchUrl = getEndpointWithQuerys("/dogs/search");
 
-    if (previousPageData && !previousPageData.next) return null;
     if (pageIndex === 0) return dogsSearchUrl;
+    if (previousPageData && !previousPageData.next) return null;
 
-    return getEndpointWithQuerys(previousPageData.next as `/${string}`);
+    const nextPage = `${import.meta.env.VITE_BASE_URL}${previousPageData.next}`;
+    return nextPage;
   };
