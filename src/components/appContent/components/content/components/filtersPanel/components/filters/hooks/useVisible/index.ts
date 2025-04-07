145793b80dbf483a4b255b261ce69994628d4dcb
ArturoDucasse <ArturoM.Ducasse@gmail.com>
2025-04-07 01:20:55 -0400

    refactor, add type to useVisible hook

--- src/components/appContent/components/content/components/filtersPanel/components/filters/hooks/useVisible/index.ts
+++ src/components/appContent/components/content/components/filtersPanel/components/filters/hooks/useVisible/index.ts
@@ -1,9 +1,9 @@
 import { useState, useRef, useEffect } from "react";
-import { UseVisibleProps } from "./types";
+import { UseVisible, UseVisibleProps } from "./types";
 
 export const useVisible = <TElement extends HTMLElement>({
   onBlur,
-}: UseVisibleProps = {}) => {
+}: UseVisibleProps = {}): UseVisible<TElement> => {
   const [isVisible, setIsVisible] = useState(false);
   const handleSetVisible = (value: boolean) => setIsVisible(value);
   const handleToggleVisible = () => setIsVisible(!isVisible);
--- src/components/appContent/components/content/components/filtersPanel/components/filters/hooks/useVisible/types/index.ts
+++ src/components/appContent/components/content/components/filtersPanel/components/filters/hooks/useVisible/types/index.ts
@@ -1,3 +1,10 @@
 export type UseVisibleProps = {
   onBlur?: () => void;
 };
+
+export type UseVisible<TElement extends HTMLElement> = {
+  isVisible: boolean;
+  handleSetVisible: (value: boolean) => void;
+  containerRef: React.RefObject<TElement | null>;
+  handleToggleVisible: () => void;
+};
