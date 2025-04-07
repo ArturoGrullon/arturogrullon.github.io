504c2b2201b993259d06960184cf52b8879afa4e
ArturoDucasse <ArturoM.Ducasse@gmail.com>
2025-04-07 01:22:27 -0400

    refactor, add animation to favorite dogs

--- src/components/appContent/components/content/components/filtersPanel/index.tsx
+++ src/components/appContent/components/content/components/filtersPanel/index.tsx
@@ -1,10 +1,13 @@
+import { AnimatePresence } from "motion/react";
 import { FavoriteDogs } from "./components/favoriteDogs";
 import { Filters } from "./components/filters";
 import { FiltersPanelProps } from "./types";
 
 export const FiltersPanel = ({ ...filters }: FiltersPanelProps) => (
   <section className="basis-[30%] flex flex-col w-full p-5">
-    <FavoriteDogs />
+    <AnimatePresence>
+      <FavoriteDogs />
+    </AnimatePresence>
     <Filters {...filters} />
   </section>
 );
