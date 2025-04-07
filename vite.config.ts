ae31a5629117fbc5108e3e68953e7e803042996d
ArturoDucasse <ArturoM.Ducasse@gmail.com>
2025-04-06 20:09:20 -0400

    refactor, update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/arturogrullon.github.io/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
